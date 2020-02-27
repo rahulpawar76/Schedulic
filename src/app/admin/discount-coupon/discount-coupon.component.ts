1c5dce4dc9efa4fc87a9454f7077737c239ad200
narinderahuja79 <bikalpitbhadani@gmail.com>
2020-02-27 13:37:36 +0530

    Kalpit

--- src/app/admin/discount-coupon/discount-coupon.component.ts
+++ src/app/admin/discount-coupon/discount-coupon.component.ts
@@ -8,15 +8,18 @@ import { Component, OnInit } from '@angular/core';
 export class DiscountCouponComponent implements OnInit {
   dtOptions: any = {};
   animal: any;
-  
-  constructor() { }
+  couponCodeListing: boolean = true;
+  addNewCouponCode: boolean = false;
+  constructor() {
+    localStorage.setItem('isBusiness', 'false');
+   }
 
-  // constructor() {
-    
-  //   localStorage.setItem('isBusiness', 'false');
-  //  }
 
   ngOnInit() {
   }
 
+  fnNewCouponCode(){
+    this.couponCodeListing = false;
+    this.addNewCouponCode = true;
+  }
 }
--- src/app/admin/settings/services/services.component.ts
+++ src/app/admin/settings/services/services.component.ts
@@ -3,7 +3,6 @@ import { Router, ActivatedRoute } from '@angular/router';
 //import { SettingsComponent } from '../settings.component';
 
 import { AuthenticationService } from '@app/_services';
-import { LoaderService } from '@app/_services/loader.service';
 
 @Component({
   selector: 'settings-services',
@@ -17,7 +16,6 @@ export class ServicesComponent implements OnInit {
     private route: ActivatedRoute,
     private router: Router,
     private authenticationService: AuthenticationService,
-    private loaderService: LoaderService,
     //private settingsComponent:SettingsComponent
   ) { }
 
