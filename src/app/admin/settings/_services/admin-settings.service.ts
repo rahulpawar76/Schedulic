3ac2a9d0c6228de699ab768ad7a7554266d70bd6
narinderahuja79 <narinderahuja79@gmail.com>
2020-03-02 18:03:26 +0530

    Krinja

--- src/app/admin/settings/_services/admin-settings.service.ts
+++ src/app/admin/settings/_services/admin-settings.service.ts
@@ -8,7 +8,7 @@ import { AuthenticationService } from '@app/_services';
 
 @Injectable({ providedIn: 'root' })
 
-export class AdminService {
+export class AdminSettingsService {
     adminId:any
     adminToken:any
     businessId : any;
@@ -34,113 +34,25 @@ export class AdminService {
        
       }
     
-    //   Business Module
-    getAllBusiness(){
+    // createNewCouponCode(createdCouponCodeData){
+    //     alert(createdCouponCodeData);
+    //     // let requestObject = {
+    //     //     'business_id': this.businessId,
+    //     // };
+    //     let headers = new HttpHeaders({
+    //         'Content-Type': 'application/json',
+    //         'admin-id' : this.adminId,
+    //         'api-token' : this.adminToken 
+    //     });
+    //     return this.http.post(`${environment.apiUrl}/create-discount-coupon`,createdCouponCodeData,{headers:headers}).pipe(
+    //     map((res) => {
+    //         return res;
+    //     }),
+    //     catchError(this.handleError));
+    // }
+    fnAllServices(){
         let requestObject = {
-        };
-        let headers = new HttpHeaders({
-            'Content-Type': 'application/json',
-            'admin-id' : this.adminId,
-            'api-token' : this.adminToken 
-        });
-        
-        return this.http.post(`${environment.apiUrl}/list-business`,requestObject,{headers:headers}).pipe(
-        map((res) => {
-            return res;
-        }),
-        catchError(this.handleError));
-    }
-    gelAllCountry(){
-        let requestObject = {
-        };
-        let headers = new HttpHeaders({
-            'Content-Type': 'application/json',
-        });
-        return this.http.post(`${environment.apiUrl}/countries`,requestObject,{headers:headers}).pipe(
-        map((res) => {
-            return res;
-        }),
-        catchError(this.handleError));
-    }
-    gelAllState(country_id){
-        let requestObject = {
-            'country_id' : country_id
-        };
-        let headers = new HttpHeaders({
-            'Content-Type': 'application/json',
-        });
-        return this.http.post(`${environment.apiUrl}/states`,requestObject,{headers:headers}).pipe(
-        map((res) => {
-            return res;
-        }),
-        catchError(this.handleError));
-    }
-    gelAllCities(state_id){
-        let requestObject = {
-            'state_id' : state_id
-        };
-        let headers = new HttpHeaders({
-            'Content-Type': 'application/json',
-        });
-        return this.http.post(`${environment.apiUrl}/cities`,requestObject,{headers:headers}).pipe(
-        map((res) => {
-            return res;
-        }),
-        catchError(this.handleError));
-    }
-    getTimeZone(){
-        let requestObject = {
-        };
-        let headers = new HttpHeaders({
-            'Content-Type': 'application/json',
-        });
-        return this.http.post(`http://api.timezonedb.com/v2.1/list-time-zone?key=L1US8PRRVKYX&format=json`,requestObject,{headers:headers}).pipe(
-        map((res) => {
-            return res;
-        }),
-        catchError(this.handleError));
-    }
-    createNewBusiness(newBusinessData){
-        let requestObject = {
-        };
-        let headers = new HttpHeaders({
-            'Content-Type': 'application/json',
-            'admin-id' : this.adminId,
-            'api-token' : this.adminToken 
-        });
-        return this.http.post(`${environment.apiUrl}/create-business`,newBusinessData,{headers:headers}).pipe(
-        map((res) => {
-           
-            return res;
-        }),
-        catchError(this.handleError));
-    }
-
-    // Appointment Module
-
-    getAllAppointments(durationType,services){
-        let requestObject = {
-            'business_id' : this.businessId,
-            'duration_type' : durationType,
-            'services' : services
-        };
-        let headers = new HttpHeaders({
-            'Content-Type': 'application/json',
-            'admin-id' : this.adminId,
-            'api-token' : this.adminToken 
-        });
-        return this.http.post(`${environment.apiUrl}/admin-booking-listing`,requestObject,{headers:headers}).pipe(
-        map((res) => {
-            return res;
-        }),
-        catchError(this.handleError));
-    }
-
-    // Get All Services
-
-    getAllServices(){
-        let requestObject = {
-            'business_id' : this.businessId,
+            'business_id': this.businessId,
         };
         let headers = new HttpHeaders({
             'Content-Type': 'application/json',
@@ -153,9 +65,7 @@ export class AdminService {
         }),
         catchError(this.handleError));
     }
-
-    // Customer Module
-    getAllCustomers(){
+    fnAllCategory(){
         let requestObject = {
             'business_id': this.businessId,
         };
@@ -164,179 +74,12 @@ export class AdminService {
             'admin-id' : this.adminId,
             'api-token' : this.adminToken 
         });
-        return this.http.post(`${environment.apiUrl}/admin-customer-list`,requestObject,{headers:headers}).pipe(
+        return this.http.post(`${environment.apiUrl}/get-all-category`,requestObject,{headers:headers}).pipe(
         map((res) => {
             return res;
         }),
         catchError(this.handleError));
     }
-    getCustomersDetails(customer_id){
-        let requestObject = {
-            'customer_id': customer_id,
-        };
-        let headers = new HttpHeaders({
-            'Content-Type': 'application/json',
-            'admin-id' : this.adminId,
-            'api-token' : this.adminToken 
-        });
-        return this.http.post(`${environment.apiUrl}/admin-customer-details`,requestObject,{headers:headers}).pipe(
-        map((res) => {
-            return res;
-        }),
-        catchError(this.handleError));
-    }
-    fnCreateNewCustomer(newCustomerData){
-        // let requestObject = {
-        //     'customer_id': customer_id,
-        // };
-        let headers = new HttpHeaders({
-            'Content-Type': 'application/json',
-            'admin-id' : this.adminId,
-            'api-token' : this.adminToken 
-        });
-        return this.http.post(`${environment.apiUrl}/admin-customer-create`,newCustomerData,{headers:headers}).pipe(
-        map((res) => {
-            return res;
-        }),
-        catchError(this.handleError));
-    }
-    fncreateNewNote(createNewNoteData){
-        // let requestObject = {
-        //     'customer_id': customer_id,
-        // };
-        let headers = new HttpHeaders({
-            'Content-Type': 'application/json',
-            'admin-id' : this.adminId,
-            'api-token' : this.adminToken 
-        });
-        return this.http.post(`${environment.apiUrl}/customer-note-create`,createNewNoteData,{headers:headers}).pipe(
-        map((res) => {
-            return res;
-        }),
-        catchError(this.handleError));
-    }
-    fnEditNote(editNoteData){
-        let headers = new HttpHeaders({
-            'Content-Type': 'application/json',
-            'admin-id' : this.adminId,
-            'api-token' : this.adminToken 
-        });
-        return this.http.post(`${environment.apiUrl}/customer-note-edit`,editNoteData,{headers:headers}).pipe(
-        map((res) => {
-            return res;
-        }),
-        catchError(this.handleError));
-    }
-
-    // Get Appointments by Category and Status
-    getAllAppointmentsByCategoryAndStatus(requestObject){
-        
-        let headers = new HttpHeaders({
-            'Content-Type': 'application/json',
-            'admin-id' : this.adminId,
-            'api-token' : this.adminToken 
-        });
-        return this.http.post(`${environment.apiUrl}/admin-today-listing`,requestObject,{headers:headers}).pipe(
-        map((res) => {
-            return res;
-        }),
-        catchError(this.handleError));
-    }
-
-    // Get Categories
-    getAllCategories(requestObject){
-        let headers = new HttpHeaders({
-          'Content-Type': 'application/json',
-            'admin-id' : this.adminId,
-            'api-token' : this.adminToken 
-        });
-
-        return this.http.post(`${environment.apiUrl}/get-all-category`,requestObject,{headers:headers} ).pipe(
-          map((res) => {
-            return res;
-          }),
-          catchError(this.handleError));
-    }
-
-    // Get 
-    get(requestObject){
-        let headers = new HttpHeaders({
-          'Content-Type': 'application/json',
-            'admin-id' : this.adminId,
-            'api-token' : this.adminToken 
-        });
-
-        return this.http.post(`${environment.apiUrl}/get-all-category`,requestObject,{headers:headers} ).pipe(
-          map((res) => {
-            return res;
-          }),
-          catchError(this.handleError));
-    }
-
-    // Get TodayRevenue
-    getTodayRevenue(requestObject){
-        let headers = new HttpHeaders({
-          'Content-Type': 'application/json',
-            'admin-id' : this.adminId,
-            'api-token' : this.adminToken 
-        });
-
-        return this.http.post(`${environment.apiUrl}/admin-today-revenue`,requestObject,{headers:headers} ).pipe(
-          map((res) => {
-            return res;
-          }),
-          catchError(this.handleError));
-    }
-
-    // Couponcode
-
-    getAllCouponCode(couponListFilter){
-        let requestObject = {
-            'business_id': this.businessId,
-            'filter' : couponListFilter,
-        };
-        let headers = new HttpHeaders({
-            'Content-Type': 'application/json',
-            'admin-id' : this.adminId,
-            'api-token' : this.adminToken 
-        });
-        return this.http.post(`${environment.apiUrl}/discount-coupon-list`,requestObject,{headers:headers}).pipe(
-        map((res) => {
-            return res;
-        }),
-        catchError(this.handleError));
-    }
-    createNewCouponCode(createdCouponCodeData){
-        alert(createdCouponCodeData);
-        // let requestObject = {
-        //     'business_id': this.businessId,
-        // };
-        let headers = new HttpHeaders({
-            'Content-Type': 'application/json',
-            'admin-id' : this.adminId,
-            'api-token' : this.adminToken 
-        });
-        return this.http.post(`${environment.apiUrl}/create-discount-coupon`,createdCouponCodeData,{headers:headers}).pipe(
-        map((res) => {
-            return res;
-        }),
-        catchError(this.handleError));
-    }
-
-     rescheduleAppointment(requestObject){
-    
-        let headers = new HttpHeaders({
-        'Content-Type': 'application/json',
-            'admin-id' : this.adminId,
-            'api-token' : this.adminToken 
-        });
-        return this.http.post(`${environment.apiUrl}/admin-booking-resedule`,requestObject,{headers:headers}).pipe(
-        map((res) => {
-          return res;
-        }),
-        catchError(this.handleError));
-      }
-
     
 
 }
