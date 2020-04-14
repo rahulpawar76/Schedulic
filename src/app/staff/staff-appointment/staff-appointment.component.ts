fd3ea1fd82f4eadfd90a167b665c1ef539e374b6
narinderahuja79 <narinderahuja79@gmail.com>
2020-04-14 17:51:42 +0530

    Krinja

--- src/app/staff/staff-appointment/staff-appointment.component.ts
+++ src/app/staff/staff-appointment/staff-appointment.component.ts
@@ -36,11 +36,11 @@ export class StaffAppointmentComponent implements OnInit {
   status: any;
   newAppointmentData: any;
   completedAppointmentData: any;
-  onGoingAppointmentData: any=[];
+  onGoingAppointmentData: any;
   notes: any;
   settingsArr:any=[];
-  cancellationBufferTime= new Date();
-  minReschedulingTime= new Date();
+  cancellationBufferTime: any;
+  minReschedulingTime: any;
   currencySymbol:any;
   currencySymbolPosition:any;
   currencySymbolFormat:any;
@@ -172,7 +172,7 @@ export class StaffAppointmentComponent implements OnInit {
         });
       }
       else if(response.data == false) {
-        this.onGoingAppointmentData = [];
+        this.onGoingAppointmentData = '';
       }
     })
   }
@@ -1445,8 +1445,7 @@ export class StaffAppointmentComponent implements OnInit {
 
       statuses: status[] = [
         {value: 'OW', viewValue: 'On The Way',statuses:''},
-        {value: 'WS', viewValue: 'Work Started',statuses:''},
-        {value: 'ITR', viewValue: 'Interrupted',statuses:''}
+        {value: 'WS', viewValue: 'Work Started',statuses:''}
       ];
 
     onNoClick(): void {
