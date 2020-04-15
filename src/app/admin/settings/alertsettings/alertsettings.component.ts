bcfbb4abe011bb1f7aaf2b1413cfc8d983bf1ecc
narinderahuja79 <bikalpitbhadani@gmail.com>
2020-04-15 16:59:43 +0530

    Kalpit

--- src/app/admin/settings/alertsettings/alertsettings.component.ts
+++ src/app/admin/settings/alertsettings/alertsettings.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Inject } from '@angular/core';
-import { AppComponent } from '@app/app.component'
+import { AppComponent } from '@app/app.component';
+import { AdminSettingsService } from '../_services/admin-settings.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 
 @Component({
@@ -11,6 +14,8 @@ export class AlertsettingsComponent implements OnInit {
   adminSettings : boolean = true;
   constructor(
     private appComponent : AppComponent,
+    public adminSettingsService : AdminSettingsService,
+    private _snackBar: MatSnackBar
     ) {
       //this.appComponent.settingsModule(this.adminSettings);
      }
