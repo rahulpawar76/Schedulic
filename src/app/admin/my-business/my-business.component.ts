2ccb3a41c189003af8445f40d11fafeaa5d717d7
narinderahuja79 <narinderahuja79@gmail.com>
2020-06-01 10:30:11 +0530

    commit(Anuj)

--- src/app/admin/my-business/my-business.component.ts
+++ src/app/admin/my-business/my-business.component.ts
@@ -68,8 +68,8 @@ export class MyBusinessComponent implements OnInit {
   }
   fnSelectBusiness(business_id,busisness_name){
 
-     localStorage.setItem('business_id', business_id);
-    // localStorage.setItem('business_id', '2');
+     // localStorage.setItem('business_id', business_id);
+    localStorage.setItem('business_id', '2');
     localStorage.setItem('business_name', busisness_name);
     this.router.navigate(['/admin/my-workspace']);
   }
