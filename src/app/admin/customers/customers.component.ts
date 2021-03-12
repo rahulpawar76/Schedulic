49ef14cceb9eae5bd05b339e9716bc166554321e
bisandip <70048264+bisandip@users.noreply.github.com>
2021-03-12 15:18:13 +0530

    print invoice

--- src/app/admin/customers/customers.component.ts
+++ src/app/admin/customers/customers.component.ts
@@ -3307,14 +3307,20 @@ onNoClick(): void {
       })
     }
 
-    fnPrint(){
-      const printContent = document.getElementById("printInvoice");
-      const WindowPrt = window.open('', '', 'left=0,top=0,width=900,height=900,toolbar=0,scrollbars=0,status=0');
-      WindowPrt.document.write(printContent.innerHTML);
-      WindowPrt.document.close();
-      WindowPrt.focus();
-      WindowPrt.print();
-      // WindowPrt.close();
+    fnPrint(){     
+      let that = this;
+      domtoimage.toPng(document.getElementById('printInvoice'))
+        .then(function (blob) {
+          const printContent = document.getElementById("printInvoice");
+          const WindowPrt = window.open('', '', 'left=0,top=0,width=900,height=900,toolbar=0,scrollbars=0,status=0');
+          WindowPrt.document.write('<img src='+blob+'>');         
+          WindowPrt.focus();
+          setTimeout(() => {
+            WindowPrt.document.close();
+            WindowPrt.print();  
+            WindowPrt.close();
+          }, 200);                                                  
+        });
     }
 
     captureScreen() {
