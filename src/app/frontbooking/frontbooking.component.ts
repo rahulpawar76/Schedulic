fa55fef6bee2d5f1bfc6855ffc4cdc1781d0747d
Kuldip <Kuldipchangani81@gmail.com>
2021-07-16 19:54:10 +0530

    changes given by client

--- src/app/frontbooking/frontbooking.component.ts
+++ src/app/frontbooking/frontbooking.component.ts
@@ -2785,7 +2785,9 @@ this.router.navigate(['/customer-login/'+this.urlString[1]]);
       
     });
     dialogRef.afterClosed().subscribe(result => {
-      
+      if(result == 'forgot-pwd'){
+        this.router.navigate(['/forgot-password']);
+      }
     });
   }
 
@@ -4343,8 +4345,7 @@ export class theme2CheckoutDialog {
     }
     
   fnForgotPWD(){
-    this.dialogRef.close();
-    this.router.navigate(['/forgot-password']);
+    this.dialogRef.close('forgot-pwd');
   }
     
   }
